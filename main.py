ce456dafa8d14145de3160a83c5bfdd65f164e94
Alexsandr <alssval@gmail.com>
2023-08-31 12:58:30 +0300

    feat(main): import button

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
 import config
